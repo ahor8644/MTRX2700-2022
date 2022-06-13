52d0bb0c802ced49765547443a9a37c10513260c
ahor8644 <101688580+ahor8644@users.noreply.github.com>
2022-06-13 14:08:10 +1000

    Update adc.c

--- week7-adc-module/Sources/adc.c
+++ week7-adc-module/Sources/adc.c
@@ -3,17 +3,20 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
 
-
+//all ADC interfaces are stored in an array --> this array is filled in by the 'RegisterADC()' function
 ADCInterface interfaces[16];
 int interface_count = 0;
 
 
 void RegisterADC(int channel_to_read, ADCHandler callback_function) {
+  //This function takes in an ADC channel to read from, as well as a callback
+  //function, and stores them in the array of ADCInterface structs using a global counter
   
-  interfaces[interface_count].channel_to_read = channel_to_read;
+  
+  interfaces[interface_count].channel_to_read = channel_to_read; 
   interfaces[interface_count].callback_function = callback_function;
   
-  interface_count += 1;
+  interface_count += 1;   //incrementing array counter 
 }
   
 
